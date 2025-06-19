c76a7b4ed9ad2e699be6b8599fd7066ece692674
João S. Barbosa <contato.jsbarbosa@gmail.com>
2025-06-19 17:44:25 -0300

    :tada: Inicializando

---
+++ src/main/java/com/joaobarbosadev/springboot_advanced_ap/SpringbootAdvancedApApplication.java
@@ -0,0 +1,13 @@
+package com.joaobarbosadev.springboot_advanced_ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdvancedApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdvancedApApplication.class, args);
+	}
+
+}
